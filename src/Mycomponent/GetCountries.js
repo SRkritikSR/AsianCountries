c100491bcf6f6dd402073ed2fae6a7dca3c9dfca
SRkritikSR <84017706+SRkritikSR@users.noreply.github.com>
2022-02-10 19:00:36 +0530

    Updated GetCountries
    Due to update in the API format

--- src/Mycomponent/GetCountries.js
+++ src/Mycomponent/GetCountries.js
@@ -1,39 +1,47 @@
 import React, { Component } from 'react'
 import { DisplayCountries } from './DisplayCountries'
-import axios from 'axios';
+
 
 export class GetCountries extends Component {
     constructor() {
         super();
         this.state = {
-            countriesList: []
+            countriesList:[]
         }
-    }
+    }   
     async componentDidMount() {
         let url = "https://restcountries.com/v3.1/region/asia?=fields=name,capital,flags,region,population,languages,borders"
         let data = await fetch(url);
         let parseDdata = await data.json();
-        // console.log(parseDdata[0].name);
+        let finalData=Object.values(parseDdata);
+        //the parsedData is in Object from, to convert it inot array and use Map function on it
+        // we convert the values of Object into a array through Object.values and the rest manipulatoin is according to the data given.
+        console.log(parseDdata[5].flags.png)
+       
+        
+        
 
 
         this.setState({
-            countriesList: parseDdata
+            countriesList: finalData
+        
         }
         )
+        console.log(this.state.countriesList);
+                
 
 
     }
+    
     render() {
         return (
             <div className="container">
                 <h1  className="text-center">COUNTRIES OF ASIA</h1>
                 <div className="row">
                     {this.state.countriesList.map((elem) => {
+                        console.log(typeof(elem.name))
                         return <div key={elem.name} className="col-md-3 my-3">
-                            <DisplayCountries Cname={elem.name} Ccapital={elem.capital} Cregion={elem.region} Cpopulation={elem.population} Cflagurl={elem.flags[1]} Clanguages={elem.languages.map((e) => {
-                                return e.name
-
-                            })} Cborders={elem.borders.map((e) => {
+                            <DisplayCountries Cname={elem.name.official} Ccapital={elem.capital} Cregion={elem.region} Cpopulation={elem.population} Cflagurl={elem.flags.png} Clanguages={Object.values(elem.languages)} Cborders={elem.borders.map((e) => {
 
                                 return e
 
@@ -51,4 +59,4 @@ export class GetCountries extends Component {
         )
 
     }
-}
+}       

71cec86632668b4d6c8b1ba32edd9cb5bc575f65
SRkritikSR <84017706+SRkritikSR@users.noreply.github.com>
2022-02-11 00:08:07 +0530

    Updated to make the countries list in ascnd ord
    Used COMPARE function  inside of array.sort to compare the countries name and arrange then ascendingly

--- src/Mycomponent/GetCountries.js
+++ src/Mycomponent/GetCountries.js
@@ -16,7 +16,22 @@ export class GetCountries extends Component {
         let finalData=Object.values(parseDdata);
         //the parsedData is in Object from, to convert it inot array and use Map function on it
         // we convert the values of Object into a array through Object.values and the rest manipulatoin is according to the data given.
-        console.log(parseDdata[5].flags.png)
+        let atemp=0;
+        finalData.sort(function (a,b) {
+            
+         //the function inside the sorting, if positive is returned then for a>b it sorts b before a(5-3 then 3,5), if negative then a before b(4-5);4,5
+         // and if zero then no change here we are comparing the letters of name of countries A>B
+                 if (a.name.common> b.name.common) {
+                     atemp=1;
+                 }
+                 else {
+                     atemp=-1;
+                 }
+           
+                 return (atemp)
+            
+        })
+        
        
         
         
@@ -27,10 +42,11 @@ export class GetCountries extends Component {
         
         }
         )
-        console.log(this.state.countriesList);
+        
+        
                 
 
-
+        
     }
     
     render() {
@@ -39,9 +55,9 @@ export class GetCountries extends Component {
                 <h1  className="text-center">COUNTRIES OF ASIA</h1>
                 <div className="row">
                     {this.state.countriesList.map((elem) => {
-                        console.log(typeof(elem.name))
+            
                         return <div key={elem.name.official} className="col-md-3 my-3">
-                            <DisplayCountries Cname={elem.name.official} Ccapital={elem.capital} Cregion={elem.region} Cpopulation={elem.population} Cflagurl={elem.flags.png} Clanguages={Object.values(elem.languages)} Cborders={elem.borders.map((e) => {
+                            <DisplayCountries Cname={elem.name.common} Ccapital={elem.capital} Cregion={elem.region} Cpopulation={elem.population} Cflagurl={elem.flags.png} Clanguages={Object.values(elem.languages)} Cborders={elem.borders.map((e) => {
 
                                 return e
 
